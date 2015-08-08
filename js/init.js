fa0fc2b44bef131e09557bc72206cc04ac7385f3
Marcus Ericsson <mericsson@gmail.com>
2015-08-07 22:12:34 -0700

    mailing list

--- js/init.js
+++ js/init.js
@@ -7,5 +7,30 @@
       $("body").scrollTo("#survey-container");
     });
 
+    function isValidEmailAddress(emailAddress) {
+    var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+    return pattern.test(emailAddress);
+};
+
+    Parse.initialize("zprZrTNffOGwa5nDLiCY3duSmjE7ms8DwHrwU1M7", "Ah7mVqGmtZDgHW399M7FaVMdQhO3ZN4sWFpgB5sY");
+
+    $("#mailingListSignUp").click(function(event) {
+      var email = $("#mailingListEmail").val();
+      if (!isValidEmailAddress(email)) {
+        alert("Please enter valid email address.");
+      } else {
+        var Contact = Parse.Object.extend("Contact");
+        var contact = new Contact();
+        contact.save({email: email}, {
+          success: function(object) {
+            alert("Thanks for signing up!");
+          },
+          error: function(model, error) {
+            alert("Sorry, unable to process sign up at this time. Please notify info@sherpaclinicalstudies.com");
+          }
+        });
+      }
+    });
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
